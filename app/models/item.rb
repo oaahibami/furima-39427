50cb1ab82816208b345ae7414282522ce1fed508
oaahibami <136575603+oaahibami@users.noreply.github.com>
2023-09-23 17:12:05 +0900

    テストコード実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,7 @@ class Item < ApplicationRecord
   belongs_to :region
   belongs_to :shipping_day
   has_one_attached :image
+  belongs_to :user
 
   validates :name, presence: true
   validates :description, presence: true
@@ -22,8 +23,12 @@ class Item < ApplicationRecord
   validates :region_id, numericality: { other_than: 1  , message: "can't be blank"}
   validates :shipping_day_id, numericality: { other_than: 1  , message: "can't be blank"}
 
-  with_options presence:true, format: { with: /\A[0-9]+\z/ } do
-    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
-               presence: { message: "can't be blank" }
-  end
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters" },
+    numericality: { 
+    only_integer: true, 
+    greater_than_or_equal_to: 300, 
+    less_than_or_equal_to: 9_999_999, 
+    message: "is out of setting range"
+  }
+
 end
