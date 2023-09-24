c3a13df0c3a62f07d022dc732dcfd4fdb33f706c
oaahibami <136575603+oaahibami@users.noreply.github.com>
2023-09-24 17:40:20 +0900

    正規表現修正1回目

--- app/models/item.rb
+++ app/models/item.rb
@@ -18,8 +18,7 @@ class Item < ApplicationRecord
   validates :region_id, numericality: { other_than: 1  , message: "can't be blank"}
   validates :shipping_day_id, numericality: { other_than: 1  , message: "can't be blank"}
 
-  validates :price, presence: true, format: { with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters" },
-    numericality: { 
+  validates :price, presence: true, numericality: { 
     only_integer: true, 
     greater_than_or_equal_to: 300, 
     less_than_or_equal_to: 9_999_999, 
